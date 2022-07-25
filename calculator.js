3b97c19e36f96a9a009ff3cba501b129a8e99561
RylanLoukusa <107429991+RylanLoukusa@users.noreply.github.com>
2022-07-25 13:50:01 -0500

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,98 +1,177 @@
 let firstNumber = ''
 let secondNumber = ''
-let currentOperation = null
+let operator = null
 
 
-const sevenBtn = document.getElementById('sevenBtn');
-const eightBtn = document.getElementById('eightBtn');
-const nineBtn = document.getElementById('nineBtn');
-const divideBtn = document.getElementById('divideBtn');
-const fourBtn = document.getElementById('fourBtn');
-const fiveBtn = document.getElementById('fiveBtn');
-const sixBtn = document.getElementById('sixBtn');
-const multiplyBtn = document.getElementById('multiplyBtn');
-const oneBtn = document.getElementById('oneBtn');
-const twoBtn = document.getElementById('twoBtn');
-const threeBtn = document.getElementById('threeBtn');
-const subtractBtn = document.getElementById('subtractBtn');
-const zeroBtn = document.getElementById('zeroBtn');
 const decimalBtn = document.getElementById('decimalBtn');
 const deleteBtn = document.getElementById('deleteBtn');
-const addBtn = document.getElementById('addBtn');
 const clearBtn = document.getElementById('clearBtn');
 const equalBtn = document.getElementById('equalBtn');
-const lastInput = document.getElecmentById('lastInput')
+const lastInput = document.getElementById('lastInput')
 const currentInput = document.getElementById('currentInput')
 
 
-function add(a,b) {
-     return a + b 
+function add(firstNumber,secondNumber) {
+     return +secondNumber + +firstNumber 
 }
-function subtract(a,b) {
-   return a - b 
+function subtract(firstNumber,secondNumber) {
+   return firstNumber - secondNumber 
 }
-function multpily(a,b) {
-    return a * b 
+function multpily(firstNumber,secondNumber) {
+    return firstNumber * secondNumber 
 }
-function divide(a,b) {
-    return a / b 
+function divide(firstNumber,secondNumber) {
+    return firstNumber / secondNumber 
 }
 
-function operate(operator, a, b) {
+
+function operate(operator, firstNumber, secondNumber) {
+    
     switch(operator) {
         case '+':
-            return add(a,b)
+            return add(firstNumber, secondNumber)
         case '-':
-            return subtract(a,b)
-        case 'x':
-            return multpily(a,b)
+            return subtract(firstNumber, secondNumber)
+        case '*':
+            return multpily(firstNumber, secondNumber)
         case '/':
-            return divide(a,b)
+            return divide(firstNumber, secondNumber)
         default:
             return null
     }
 
 }
 
-equalsButton.addEventListener('click', evaluate)
-deleteButton.addEventListener('click', deleteNumber)
-clearButton.addEventListener('click', clear)
+multiplyBtn.addEventListener('click', inputMultiply)
+addBtn.addEventListener('click', inputAdd)
+subtractBtn.addEventListener('click', inputSubtract)
+divideBtn.addEventListener('click', inputDivide)
+decimalBtn.addEventListener('click',addDecimal)
+
+function inputMultiply() {
+    if (operator !== null) evaluate()
+    lastInput.textContent = currentInput.textContent + ' ' + document.getElementById("multiplyBtn").value;
+    firstNumber = currentInput.textContent
+    currentInput.textContent = ''
+    operator = '*'
+}
+function inputAdd() {
+    if (operator !== null) evaluate()
+    lastInput.textContent = currentInput.textContent + ' ' + document.getElementById("addBtn").value;
+    firstNumber = currentInput.textContent
+    currentInput.textContent = ''
+    operator = '+'   
+}
+function inputSubtract() {
+    if (operator !== null) evaluate()
+    lastInput.textContent = currentInput.textContent + ' ' + document.getElementById("subtractBtn").value;
+    firstNumber = currentInput.textContent
+    currentInput.textContent = ''
+    operator = '-'
+}
+function inputDivide() {
+    if (operator !== null) evaluate()
+    lastInput.textContent = currentInput.textContent + ' ' + document.getElementById("divideBtn").value;
+    firstNumber = currentInput.textContent
+    currentInput.textContent = ''
+    operator = '/'
+}
+equalBtn.addEventListener('click', evaluate)
+deleteBtn.addEventListener('click', deleteNumber)
+clearBtn.addEventListener('click', clear)
+
+sevenBtn.addEventListener('click', inputSeven)
+eightBtn.addEventListener('click', inputEight)
+nineBtn.addEventListener('click', inputNine)
+sixBtn.addEventListener('click', inputSix)
+fiveBtn.addEventListener('click', inputFive)
+fourBtn.addEventListener('click', inputFour)
+threeBtn.addEventListener('click', inputThree)
+twoBtn.addEventListener('click', inputTwo)
+oneBtn.addEventListener('click', inputOne)
+zeroBtn.addEventListener('click', inputZero)
 
 function clear() {
-    currentInput.textContent = '0'
+    currentInput.textContent = ''
     lastInput.textContent = ''
-    firstNumber = ''
     secondNumber = ''
-    currentInput = null
+    firstNumber = ''
+    
+       
 }
 function evaluate() {
+    if(operator === null){
+        return
+    }
+    lastInput.textContent = lastInput.textContent + ' ' + currentInput.textContent + ' ='
     secondNumber = currentInput.textContent
-    currentInput.textContent = operate(currentInput, firstNumber, secondNumber)
-
-    lastInput.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`
-    currentOperation = null
+    currentInput.textContent = ''
+    if(operator === '/' && secondNumber === '0'){
+        alert('Error: You can not divide by 0')
+        currentInput.textContent = ''
+        lastInput.textContent = ''
+        return
+    }
+    if(secondNumber === ''){
+        return '0'
+    }
+    currentInput.textContent = currentInput.textContent + operate(operator, firstNumber, secondNumber);
+    operator = null
+    
 }
 function deleteNumber() {
     currentInput.textContent = currentInput.textContent.toString().slice(0, -1)
 }
-
-sevenBtn.addEventListener('click', inputNumber)
-eightBtn.addEventListener('click', inputNumber)
-nineBtn.addEventListener('click', inputNumber)
-fourBtn.addEventListener('click', inputNumber)
-fiveBtn.addEventListener('click', inputNumber)
-sixBtn.addEventListener('click', inputNumber)
-threeBtn.addEventListener('click', inputNumber)
-twoBtn.addEventListener('click', inputNumber)
-oneBtn.addEventListener('click', inputNumber)
-zeroBtn.addEventListener('click', inputNumber(button.textContent))
-
-function inputNumber(number) {
-    if(currentInput === '0' ){
-        resetScreen()
-    }
-    currentInput.textContent += number
-}
 function resetScreen() {
     currentInput.textContent = ''
 }
+function addDecimal() {
+    if(currentInput.textContent === '') {
+        currentInput.textContent = currentInput.textContent + '0.'
+    }
+    else {currentInput.textContent = currentInput.textContent + '.'}
+}
+
+
+function inputSeven () {
+    currentInput.textContent += document.getElementById("sevenBtn").value;
+}
+
+function inputEight () {
+    currentInput.textContent += document.getElementById("eightBtn").value;
+}
+
+function inputNine () {
+    currentInput.textContent += document.getElementById("nineBtn").value;
+}
+
+function inputSix () {
+    currentInput.textContent += document.getElementById("sixBtn").value;
+}
+
+function inputFive () {
+    currentInput.textContent += document.getElementById("fiveBtn").value;
+}
+
+function inputFour () {
+    currentInput.textContent += document.getElementById("fourBtn").value;
+}
+
+function inputThree () {
+    currentInput.textContent += document.getElementById("threeBtn").value;
+}
+
+function inputTwo () {
+    currentInput.textContent += document.getElementById("twoBtn").value;
+}
+
+function inputOne () {
+    currentInput.textContent += document.getElementById("oneBtn").value;
+}
+
+function inputZero () {
+    currentInput.textContent += document.getElementById("zeroBtn").value;
+}
+
+
+
